bd5bb5f6731565d1a2c7e02b9747febeb197526d
dduniverse <suhyunbory@gmail.com>
2022-08-14 21:09:41 +0900

    [Silver V] Title: Base Conversion, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11576. Base Conversion/Base Conversion.py
@@ -0,0 +1,20 @@
+a,b=map(int,input().split()) # a진법, b진법
+m=int(input()) # a진법을 이루고 있는 숫자 m개
+m_list=list(map(int,input().split()))
+
+a_num=[range(0,a)] 
+b_num=[range(0,b)]
+
+sum=0 # a진법 -> 10진법으로 바꾼 수
+for i in range(m):
+    sum+=m_list[i]*(a**(m-i-1))
+
+result=[]
+while sum:
+    result.append(str(sum%b)) # join을 위해 나머지를 문자열로 저장
+    sum=sum//b
+
+result=result[::-1]
+result=' '.join(result) # 출력시 띄어쓰기를 해주기 위해 공백을 기준으로 join
+
+print(result)
