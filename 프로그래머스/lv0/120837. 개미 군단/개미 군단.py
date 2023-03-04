0b1da32974a85b01fc3c126ad9d7550df76916a9
dduniverse <suhyunbory@gmail.com>
2023-03-04 16:01:14 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,15 @@
+def solution(hp):
+    answer = 0
+    
+    while(hp != 0):
+        if hp // 5 > 0:
+            answer += hp // 5
+            hp = hp % 5
+        elif hp // 3 > 0:
+            answer += hp // 3
+            hp = hp % 3
+        else:
+            answer += hp // 1
+            break
+    
+    return answer
