b736597bcccf63512964c12d808410ed8d1b66d3
dduniverse <suhyunbory@gmail.com>
2023-03-05 19:03:54 +0900

    [level 0] Title: 공 던지기, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.py
@@ -0,0 +1,9 @@
+def solution(numbers, k):
+    answer = 0
+    if len(numbers) < k * 2:
+        numbers = numbers * ((k*2) // len(numbers) + 1)
+        print(numbers)
+
+    answer = numbers[2*(k-1)]
+        
+    return answer
