8bcf9ab855713642c00d7c45c02b6481a8ed6028
dduniverse <suhyunbory@gmail.com>
2023-03-09 14:49:18 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,5 +1,4 @@
 def solution(cipher, code):
     answer = ''
-    for i in range(1, (len(cipher)//code) + 1):
-        answer += cipher[(i * code) - 1]
+    answer = cipher[code-1::code]
     return answer
