75ccae0b497242cde48ef4dcccca46c87bca6143
dduniverse <suhyunbory@gmail.com>
2023-03-15 15:45:49 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,7 +1,7 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
+    for i in range(1, n+1): # n의 3x숫자를 알기 위해 1부터 n까지의 수를 탐색
         answer += 1
-        while ('3' in str(answer)) or (answer % 3 == 0):
+        while ('3' in str(answer)) or (answer % 3 == 0): # 3이 포함되어 있거나 3의 배수이면 +1을 해줌
             answer += 1
     return answer
