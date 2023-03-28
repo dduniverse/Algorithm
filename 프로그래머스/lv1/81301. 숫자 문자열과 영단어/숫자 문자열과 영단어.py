7d848a095229f4ab44973afd5b18a1137ec19839
dduniverse <suhyunbory@gmail.com>
2023-03-28 15:23:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    ndict = {'zero':'0', 'one':'1', 'two':'2', 'three':'3','four':'4',
+           'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9'}
+    
+    for i in ndict:
+        if i in s:
+            s = s.replace(i, ndict.get(i))
+    return int(s)
