1b68cc5378bd41e7e145b8909363706331388848
dduniverse <suhyunbory@gmail.com>
2023-04-06 17:46:35 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,10 @@
+def solution(name, yearning, photo):
+    nydict = dict(zip(name, yearning))
+    for pho in photo:
+        for p in range(len(pho)):
+            if pho[p] in nydict:
+                pho[p] = nydict[pho[p]]
+            else:
+                pho[p] = 0
+    
+    return [sum(pho) for pho in photo]
