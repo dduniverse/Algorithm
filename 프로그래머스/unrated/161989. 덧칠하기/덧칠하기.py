368bac5206a1d591213175359fdfcf8dede7bbaa
dduniverse <suhyunbory@gmail.com>
2023-05-02 20:25:46 +0900

    [unrated] Title: 덧칠하기, Time: 2.67 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -3,7 +3,7 @@ def solution(n, m, section):
     paint = section[0]
     for i in range(1, len(section)):
         if section[i] - paint >= m:
-            paint = section[i]
             answer += 1
+            paint = section[i]
             
     return answer 
