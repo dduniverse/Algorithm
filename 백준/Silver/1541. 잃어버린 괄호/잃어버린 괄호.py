536de1f012a6eccbafbc53b1c05b662c89aed7a4
dduniverse <suhyunbory@gmail.com>
2023-05-08 18:22:58 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,12 @@
+math = input().split('-') # '-' 기준 분리
+
+hap = []
+for i in math: # '+' 기준 분리하여 합을 구함
+    temp = list(map(int, i.split('+'))) 
+    hap.append(sum(temp))
+
+result = hap[0] 
+for i in hap[1:]: # hap의 1번 이후 값들을 순차적으로 result에서 빼줌
+    result -= i
+
+print(result)
