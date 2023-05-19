10686cd69a12ea32d96444725b591c93de43abff
dduniverse <suhyunbory@gmail.com>
2023-05-19 14:45:25 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.36 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,43 +1,44 @@
 def solution(numbers, hand):
-    key = {0: [0, 4, 3, 4, 3, 2, 3, 2, 1, 2], # 각 키에서 0~9키 까지의거리 
-           1: [4, 0, 1, 2, 1, 2, 3, 2, 3, 4],
-           2: [3, 1, 0, 1, 2, 1, 2, 3, 2, 3],
-           3: [4, 2, 1, 0, 3, 2, 1, 4, 3, 2],
-           4: [3, 1, 2, 3, 0, 1, 2, 1, 2, 3],
-           5: [2, 2, 1, 2, 1, 0, 1, 2, 1, 2],
-           6: [3, 3, 2, 1, 2, 1, 0, 3, 2, 1],
-           7: [2, 2, 3, 4, 1, 2, 3, 0, 1, 2],
-           8: [1, 3, 2, 3, 2, 1, 2, 1, 0, 1], 
-           9: [2, 4, 3, 2, 3, 2, 1, 2, 1, 0],
-           '*': [1, 3, 4, 5, 2, 3, 4, 1, 2, 3],
-           '#': [1, 5, 4, 3, 4, 3, 2, 3, 2, 1]
-          } 
-    left, right = '*', '#'
+    key = {1: [0, 0],
+           2: [0, 1],
+           3: [0, 2],
+           4: [1, 0],
+           5: [1, 1],
+           6: [1, 2],
+           7: [2, 0],
+           8: [2, 1], 
+           9: [2, 2],
+           0: [3, 1],
+           '*': [3, 0],
+           '#': [3, 2]
+          } # 각 키의 좌표
+    
+    left, right = key['*'], key['#']
     result = ''
     for i in numbers:
         if i in [1, 4, 7]:
             result += 'L'
-            left = i
+            left = key[i]
         elif i in [3, 6, 9]:
             result += 'R'
-            right = i
+            right = key[i]
         else:
-            ll = key[left][i]
-            rr = key[right][i]
+            num = key[i]
+            ll = abs(left[0] - num[0]) + abs(left[1] - num[1])
+            rr = abs(right[0] - num[0]) + abs(right[1] - num[1])
             if ll == rr:
-                if hand == 'right':
-                    right = i
-                    result += 'R'
-                else:
-                    left = i
+                if hand == 'left':
+                    left = key[i]
                     result += 'L'
+                else:
+                    right = key[i]
+                    result += 'R'
             elif ll < rr:
-                left = i
+                left = key[i]
                 result += 'L'
             else:
-                right = i
-                result +='R'
-                
+                right = key[i]
+                result += 'R'
+    
     return result
-                
             
