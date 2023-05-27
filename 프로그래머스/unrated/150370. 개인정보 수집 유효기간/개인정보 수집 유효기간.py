3591bb0b7cdba09bc8abc504b6ef4b51d3ca82f8
dduniverse <suhyunbory@gmail.com>
2023-05-27 16:34:02 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -2,28 +2,16 @@ def solution(today, terms, privacies):
     today = list(map(int, today.split('.'))) # [0]연, [1]월, [2]일
     terms = {i[0]: int(i[2:])for i in terms}
     
+    today = today[0] * 28 * 12 + today[1] * 28 + today[2] # 일 단위로 변환
+    
     answer = []
     for i, pri in enumerate(privacies):
         day, kind = pri.split()
+        month = terms[kind]
         day = list(map(int, day.split('.')))
-        month = terms[kind] # 유효기간
+        day = day[0] * 28 * 12 + day[1] * 28 + day[2]-1 + month * 28 # 일 단위로 변환
         
-        day[1] += month
-        while day[1] > 12:
-            day[1] -= 12
-            day[0] += 1
-        
-        day[2] -= 1        
-        if day[2] == 0:
-            day[2] = 28
-            day[1] -= 1
-        print(day)   
-        
-        if day[0] < today[0]:
-            answer.append(i+1)
-        elif day[0] == today[0] and day[1] < today[1]:
-            answer.append(i+1)
-        elif day[0] == today[0] and day[1] == today[1] and day[2] < today[2]:
+        if day < today:
             answer.append(i+1)
                     
     return answer
