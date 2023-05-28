5e38c1fff67593784870dfaf58cde76d9c118d82
dduniverse <suhyunbory@gmail.com>
2023-05-28 21:48:12 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,5 +1,5 @@
 def solution(s):
-    s_list = s.split(' ')
+    s_list = s.split(' ') # 공백(' ') 기준 분리 -> 공백문자가 연속으로 나올 수 있기 때문에 기준split 기준을 명확히 해주어야 함
     for i in range(len(s_list)):
         s_list[i] = s_list[i].capitalize()
     return ' '.join(s_list)
