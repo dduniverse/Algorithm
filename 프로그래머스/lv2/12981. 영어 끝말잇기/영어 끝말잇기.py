65365e1d6f4e29d11b02e2cb2e1529c1426f7ecf
dduniverse <suhyunbory@gmail.com>
2023-06-02 19:09:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,14 +1,17 @@
 def solution(n, words):
     for i in range(1, len(words)):
         last = words[i-1][-1]  # 마지막 글자
-        if last != words[i][0]: # 앞사람과 다른 글자로 시작
-             return [(i % n) + 1, (i // n) + 1]
+        #if last != words[i][0]: # 앞사람과 다른 글자로 시작
+        #     return [(i % n) + 1, (i // n) + 1]
             
-        if len(words[i]) == 1: # 한글자일 때
-            return [(i % n) + 1, (i // n) + 1]
+        #if len(words[i]) == 1: # 한글자일 때
+        #    return [(i % n) + 1, (i // n) + 1]
         
-        if words[i] in words[:i]: # 중복되는 단어일 때
-            return [(i % n) + 1, (i // n) + 1]    
-    else:
-        return [0, 0]        
+        #if words[i] in words[:i]: # 중복되는 단어일 때
+        #    return [(i % n) + 1, (i // n) + 1]  
+        
+        if last != words[i][0] or len(words[i]) == 1 or words[i] in words[:i]:
+            return [(i % n) + 1, (i // n) + 1]  
+
+    return [0, 0]        
         
