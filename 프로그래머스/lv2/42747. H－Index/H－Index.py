2de614eabdead7bd1e9a8e2f33e0125f4e444d9a
dduniverse <suhyunbory@gmail.com>
2023-06-11 15:52:08 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,6 +1,4 @@
 def solution(citations):
-    citations.sort(reverse=True)
-    
     for h in range(max(citations), 0, -1): # h(범위: 최대 인용 횟수부터 1까지) '역순' 순회
         count = 0  # h번 이상 인용된 논문 수
         for c_num in citations:
