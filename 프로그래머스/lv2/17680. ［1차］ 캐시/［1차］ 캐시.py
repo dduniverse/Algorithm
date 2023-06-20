972e87878b131b39f5f4327a7bf499ff8b21531e
dduniverse <suhyunbory@gmail.com>
2023-06-20 23:03:25 +0900

    [level 2] Title: [1차] 캐시, Time: 0.78 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,20 @@
+def solution(cacheSize, cities):
+    cache = [] # 캐시
+    time = 0 # 총 실행시간
+    if cacheSize > 0:
+        for city in cities:
+            city = city.lower()
+            if city in cache: # 캐시에 존재할 때(cache hit) 최근 위치로 갱신
+                cache.remove(city)
+                cache.append(city)
+                cache = cache[:cacheSize]
+                time += 1
+            else: # 캐시에 존재하지 않으면(cache miss)
+                cache.append(city)
+                cache = cache[-cacheSize:]
+                time += 5        
+        return time
+    
+    else:
+        return len(cities) * 5
+        
