5219fdfef824c06d82967a05e52a37dd5e1b53cb
dduniverse <suhyunbory@gmail.com>
2023-06-23 21:45:07 +0900

    [level 2] Title: 주식가격, Time: 20.40 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,15 +1,16 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0 for _ in range(n)]
-    stack = []
-    for i, price in enumerate(prices):
-        while stack and stack[-1][1] > price:
-            idx, p = stack.pop()
-            answer[idx] += i - idx
-            
-        stack.append((i, price))
+    answer = [0 for _ in range(len(prices))]
     
-    for i, price in stack:
-        answer[i] = n-(i+1)
+    stack = [] # 상승중인 가격을 저장하는 스택
+    for i, price in enumerate(prices):
+        # stack에 있는 가격들이 지금 들어온 price보다 작으면 계속해서 실행(while)
+        while stack and stack[-1][1] > price: # 스택 존재 &, 마지막 가격이 현재가보다 높으면
+            idx, p = stack.pop() # 스택에서 pop
+            answer[idx] += i - idx # 몇초간 가격이 떨어지지 않는지 계산
+            
+        stack.append((i, price)) # 가격 계산이 끝나면 스택에 현재가 추가
+    
+    for i, price in stack: # 스택에 남아있는 가격들에 대해 유지되는 시간 계산
+        answer[i] = len(prices)-(i+1)
         
     return answer
