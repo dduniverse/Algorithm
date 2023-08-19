cd3f0299fcdb3f7bf47e287f3ccd419ff88a1d47
dduniverse <suhyunbory@gmail.com>
2023-08-19 19:33:25 +0900

    [Silver III] Title: 소수 구하기, Time: 452 ms, Memory: 72348 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,20 @@
 import math
 
-def yaksoo(n):
-    if n==1: # 1은 약수가 아니므로 제외
-        return False
-    else:
-        for i in range(2,int(math.sqrt(n))+1): # n의 제곱근까지 약수가 존재하는지 확인(** 에라토스테네스의 체 참고)
-            if n%i==0: # 약수가 존재하면 False
-                return False
-        return True # 존재하지 않으면 True
+m, n = map(int, input().split())  # 범위
+# 0부터 n까지의 배열
+nums = [0, 0]
+for i in range(2, n+1):
+    nums.append(i)
+    
+for i in range(2, int(math.sqrt(n))+1):
+    # 이미 지운 수는 다시 지우지 않음
+    if nums[i] == 0: continue
+    
+    # i의 배수 제거(i는 지우지 않으므로 2배수부터 탐색)
+    for j in range(i*2, n+1, i):
+        nums[j] = 0
 
-m,n=map(int,input().split())
-for i in range(m,n+1):
-    if yaksoo(i): # True이면 print 실행
-        print(i)
+# m이상 n이하의 소수 출력        
+for i in range(m, n+1):
+    if nums[i] != 0:
+        print(nums[i])
