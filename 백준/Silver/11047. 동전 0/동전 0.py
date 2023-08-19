6b62e60f4de902b993095df7b8ef793aca412d9e
dduniverse <suhyunbory@gmail.com>
2023-08-19 18:54:59 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,12 +1,10 @@
-n, k = map(int, input().split())
-array = []
-for i in range(n):
-    array.append(int(input()))
+n, k = map(int, input().split()) # 동전 n개, K원
+coins = [int(input()) for _ in range(n)] # 동전의 가치(오름차순 입력)
+coins.sort(reverse=True) # 내림차순 정렬
 
-coin = 0 # 동전의 개수
-for i in range(1, n+1): # array의 뒤에서부터(=큰 값부터)
-    if (k != 0) and ((k // array[-i]) > 0): # k가 0이 아니고, array[i]로 나눈 몫이 존재하면
-        coin += (k // array[-i]) # 몫을 coin에 누적
-        k %= array[-i] # k를 나머지로 바꿈
+c = 0  # 필요한 동전 개수의 최솟값
+for coin in coins:
+    c += k // coin  # 몫
+    k %= coin  # 나머지
 
-print(coin)
+print(c)
