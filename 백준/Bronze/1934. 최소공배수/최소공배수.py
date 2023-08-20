f81b1b4c9649ef44ebff5013ffd2e5f930ce5327
dduniverse <suhyunbory@gmail.com>
2023-08-20 14:56:26 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,7 +1,14 @@
-import math
-
-t=int(input())
-
+t = int(input()) # 테스트 케이스의 개수
 for i in range(t):
-    a,b=map(int,input().split())
-    print(math.lcm(a,b))
+    a, b = map(int, input().split())
+    
+    ab = a * b
+    
+    # 최대 공약수 구하기(유클리드 호제법)
+    while a % b > 0:
+        a, b = b, a % b   
+    gcd = b
+    
+    # 최소 공배수 = a*b / gcd
+    lcm = ab // gcd
+    print(lcm)
