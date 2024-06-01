fbad4075117375df862f674e78a100f21960cb68
dduniverse <suhyunbory@gmail.com>
2024-06-01 21:27:55 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,14 +1,5 @@
 function solution(a, b) {
-    var answer = 0;
-    
-    const case1 = Number(String(a) + String(b));
-    const case2 = Number(String(b) + String(a));
-    
-    if (case1 >= case2) {
-        answer = case1;
-    } else {
-        answer = case2;
-    }
-    
+    var answer = 0;   
+    answer = Math.max(Number(String(a) + String(b)), Number(String(b) + String(a)))
     return answer;
 }
